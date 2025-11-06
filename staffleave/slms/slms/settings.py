035e14b9c8443705279f2e84735b45c32496a692
Bilaalofficial <bilaalofficial1@gmail.com>
2025-11-06 18:17:44 +0530

    added jenkins pipeline

--- staffleave/slms/slms/settings.py
+++ staffleave/slms/slms/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8j+nu3y0_ab%bhda_(x3=8&!a+s^!czew#-b2m#1m(-g#$uaya
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.232.91.247', 'localhost', '127.0.0.1']
+# ALLOWED_HOSTS = []
 
 
 
